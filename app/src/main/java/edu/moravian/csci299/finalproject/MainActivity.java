e5f33e10f78cb567ef0c4371454beee0fddd9047
evan-toyberg <78161344+evan-toyberg@users.noreply.github.com>
2021-05-01 22:21:36 -0400

    Add calendar

--- app/src/main/java/edu/moravian/csci299/finalproject/MainActivity.java
+++ app/src/main/java/edu/moravian/csci299/finalproject/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
--- app/src/main/java/edu/moravian/csci299/finalproject/ScheduleFragment.java
+++ app/src/main/java/edu/moravian/csci299/finalproject/ScheduleFragment.java
@@ -1,22 +1,37 @@
 package edu.moravian.csci299.finalproject;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class ScheduleFragment extends Fragment {
-    public ScheduleFragment() {
-// Required empty public constructor
-    }
+    private CalendarView calendarView;
+    private List<EventDay> eventDays = new ArrayList<>();
+
+    public ScheduleFragment() { } // Required empty constructor
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
@@ -25,8 +40,27 @@ public class ScheduleFragment extends Fragment {
 // Inflate the layout for this fragment
         View base = inflater.inflate(R.layout.schedule_fragment, container, false);
 
+        calendarView = (CalendarView) base.findViewById(R.id.calendarView);
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Calendar clickedDayCalendar = eventDay.getCalendar();
+                try {
+                    calendarView.setDate(clickedDayCalendar);
+//                    calendarView.set
+                } catch (OutOfDateRangeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
         return base;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
     }
 }
